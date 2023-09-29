62de241e5b0c8eef310cd7c0375fd26442eb9441
claye <clayestokes@gmail.com>
2023-09-29 17:22:55 -0400

    Adding project files to github

---
+++ src/data/designWorks1.js
@@ -0,0 +1,354 @@
+import imgT1 from "../assets/image/jpg/arrangements/sage-flowers-1.jpg";
+import imgT2 from "../assets/image/jpg/arrangements/sage-flowers-2.jpg";
+import imgT3 from "../assets/image/jpg/arrangements/sage-flowers-3.jpg";
+import imgT4 from "../assets/image/jpg/arrangements/sage-flowers-4.jpg";
+import imgT5 from "../assets/image/jpg/arrangements/sage-flowers-5.jpg";
+import imgT6 from "../assets/image/jpg/arrangements/sage-flowers-6.jpg";
+import imgT7 from "../assets/image/jpg/arrangements/sage-flowers-7.jpg";
+import imgT8 from "../assets/image/jpg/arrangements/sage-flowers-8.jpg";
+import imgT9 from "../assets/image/jpg/arrangements/sage-flowers-9.jpg";
+import imgT10 from "../assets/image/jpg/arrangements/sage-flowers-10.jpg";
+import imgT11 from "../assets/image/jpg/arrangements/sage-flowers-11.jpg";
+import imgT12 from "../assets/image/jpg/arrangements/sage-flowers-12.jpg";
+import imgT13 from "../assets/image/jpg/arrangements/sage-flowers-13.jpg";
+import imgT14 from "../assets/image/jpg/arrangements/sage-flowers-14.jpg";
+import imgT15 from "../assets/image/jpg/arrangements/sage-flowers-15.jpg";
+import imgT16 from "../assets/image/jpg/arrangements/sage-flowers-16.jpg";
+import imgT17 from "../assets/image/jpg/arrangements/sage-flowers-17.jpg";
+import imgT18 from "../assets/image/jpg/arrangements/sage-flowers-18.jpg";
+import imgT19 from "../assets/image/jpg/arrangements/sage-flowers-19.jpg";
+import imgT20 from "../assets/image/jpg/arrangements/sage-flowers-20.jpg";
+import imgT21 from "../assets/image/jpg/arrangements/sage-flowers-21.jpg";
+import imgT22 from "../assets/image/jpg/arrangements/sage-flowers-22.jpg";
+import imgT23 from "../assets/image/jpg/arrangements/sage-flowers-23.jpg";
+import imgT24 from "../assets/image/jpg/arrangements/sage-flowers-24.jpg";
+import imgT25 from "../assets/image/jpg/arrangements/sage-flowers-25.jpg";
+import imgT26 from "../assets/image/jpg/arrangements/sage-flowers-26.jpg";
+import imgT27 from "../assets/image/jpg/arrangements/sage-flowers-27.jpg";
+import imgT28 from "../assets/image/jpg/arrangements/sage-flowers-28.jpg";
+import imgT29 from "../assets/image/jpg/arrangements/sage-flowers-29.jpg";
+import imgT30 from "../assets/image/jpg/arrangements/sage-flowers-30.jpg";
+import imgT31 from "../assets/image/jpg/arrangements/sage-flowers-31.jpg";
+import imgT32 from "../assets/image/jpg/arrangements/sage-flowers-32.jpg";
+import imgT33 from "../assets/image/jpg/arrangements/sage-flowers-33.jpg";
+import imgT34 from "../assets/image/jpg/arrangements/sage-flowers-34.jpg";
+import imgT35 from "../assets/image/jpg/arrangements/sage-flowers-35.jpg";
+import imgT36 from "../assets/image/jpg/arrangements/sage-flowers-36.jpg";
+import imgT37 from "../assets/image/jpg/arrangements/sage-flowers-37.jpg";
+import imgT38 from "../assets/image/jpg/arrangements/sage-flowers-38.jpg";
+
+export const designWorks1 = [
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`],
+    thumbnail: imgT5,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`],
+    thumbnail: imgT6,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Halloween`, `Fall`],
+    thumbnail: imgT7,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`],
+    thumbnail: imgT8,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Fall`],
+    thumbnail: imgT9,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Halloween`],
+    thumbnail: imgT2,
+  link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Halloween`],
+  thumbnail: imgT1,
+link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Fall`],
+    thumbnail: imgT3,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`],
+    thumbnail: imgT4,
+  link : "/portfolio-details"
+},
+  
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`],
+    thumbnail: imgT10,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Centerpieces`],
+    thumbnail: imgT11,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Centerpieces`, `Fall`],
+    thumbnail: imgT12,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`, `Fall`],
+    thumbnail: imgT13,
+  link : "/portfolio-details"
+},
+  {
+    brand: `Sage Flowers`,
+    slug: `arrangementId`,
+    title: ``,
+    categories: [`Decor`],
+    thumbnail: imgT14,
+  link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT14,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT15,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT16,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT17,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT18,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT19,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT20,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT21,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT22,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT23,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT24,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT25,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT26,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT27,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT28,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT29,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT30,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT31,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT32,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT33,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT34,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT35,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Fall`],
+  thumbnail: imgT36,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`],
+  thumbnail: imgT37,
+link : "/portfolio-details"
+},
+{
+  brand: `Sage Flowers`,
+  slug: `arrangementId`,
+  title: ``,
+  categories: [`Decor`, `Centerpieces`],
+  thumbnail: imgT38,
+link : "/portfolio-details"
+},
+];
